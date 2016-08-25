f54a32a90eef09ab32f9ea26020cdc524af03ce3
Dustin <dustin.zeigler@fullsail.edu>
2016-08-25 01:39:03 -0400

    Removed logging to a file and added the use of console.error() and console.warn()

--- src/fs-utool.js
+++ src/fs-utool.js
@@ -1,4 +1,4 @@
-exports.debug = (title, obj) => {
+exports.debug = (title, obj, level = undefined) => {
   const colors = require('colors');
   const fs = require('fs');
 
@@ -14,9 +14,15 @@ exports.debug = (title, obj) => {
   const output = seperator + title + JSON.stringify(obj) + '\n' + time + seperator;
 
   if (process.env.DEBUG) {
-    fs.appendFile('lib/logs/eLog.log', output, 'utf8', (err) => {
-      if (err) throw err;
-    });
-    console.log(output);
+    switch (level) {
+      case 2:
+        console.error(output);
+        break;
+      case 1:
+        console.warn(output);
+        break;
+      default:
+        console.log(output);
+    }
   }
 };
